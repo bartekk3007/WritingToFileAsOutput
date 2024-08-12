a3348e73b1517e0fd79c904b6e097d7b12c08fd4
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-08-12 10:19:21 +0200

    Add files via upload

---
+++ WritingFileAsOutput/src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String[] names = {"Ala ma kota", "Kot ma Ale", "Czesław zna Daniela", "Daniel jest znany przez Czesława"};
+        for(int i = 0; i < names.length; i++)
+        {
+            System.out.println(names[i]);
+        }
+    }
+}
